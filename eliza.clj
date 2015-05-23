a4d1e3db20b04bc56f4e441e733236717f85c0c2
sach <sacha.saint-leger@mail.mcgill.ca>
2015-05-23 10:12:39 -0400

    added pattern-matching to eliza

--- eliza.clj
+++ eliza.clj
@@ -11,5 +11,17 @@
     (and (simple-equal (first x) (first y))
          (simple-equal (rest x) (rest y)))))
 
+(defn variable-p [x]
+  ; is x a variable (a symbol beginning with '?')?
+  (and (symbol? x) (.startsWith (name x) "?")))
+       ; check the clojure equivalent of symbol-name
 
-
+(defn pat-match [pattern input]
+  ;Does pattern match input? Any variable can match anything.
+  (if (variable-p pattern)
+    true
+    (if  (or (or (single-valued? pattern) (= () pattern)) (or (single-valued? input) (= () input)))
+      (= pattern input)
+      (and (pat-match (first pattern) (first input))
+           (pat-match (rest pattern) (rest input))))))
+    
