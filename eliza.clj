b02583c971800ac4eb3d4fd24dc746b9704f2cd2
sach <sacha.saint-leger@mail.mcgill.ca>
2015-05-22 23:39:05 -0400

    started working on eliza lisp to clojure translation

---
+++ eliza.clj
@@ -0,0 +1,16 @@
+                                        ;eliza clojure implementation
+(def single-valued? (complement coll?))
+ ; no such thing as atom predicate in clojure (atom means a completely different thing)
+ ; collection is essentially the inverse of atom
+
+(defn simple-equal [x y]
+  ; Are x and y equal? (Don't check inside strings.)
+  (if  (or (or (single-valued? x) (= () x)) (or (single-valued? y) (= () y)))
+    (= x y)
+    (and (simple-equal (first x) (first y))
+         (simple-equal (rest x) (rest y)))))
+
+
+;(or (single-valued? x) (= () x))
+
+   ;seems like () might be posing a problem
